2a62583eb789df7c0957b322611aa57a017acc9b
BILL GRAHAM <grahambill011@gmail.com>
2021-10-09 04:16:29 +0300

    Update my_home_page.dart

--- flutter_payment_app/lib/pages/my_home_page.dart
+++ flutter_payment_app/lib/pages/my_home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_payment_app/component/colors.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -10,6 +12,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double h=MediaQuery.of(context).size.height;
+    double w=MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: AppColor.backGroundColor,
+      body: Container(
+        child: Stack(
+          children: [
+
+          ],
+        ),
+      ),
+    );
   }
 }
