68bbc1c9ddd3cab68a57dad216bb6e25d73c8256
BILL GRAHAM <grahambill011@gmail.com>
2021-10-09 04:08:00 +0300

    Update main.dart

--- flutter_payment_app/lib/main.dart
+++ flutter_payment_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_payment_app/pages/my_home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,18 +13,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Container(color:Colors.white)
+      home: MyHomePage(),
     );
   }
 }
